d8d6804689fc95424a4229217bb437fad70edd4b
programer1983 <programer972@gmail.com>
2021-12-23 17:45:48 +0200

    creating a top bar;

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {useState} from "react"
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
@@ -8,9 +9,12 @@ import "./app.scss"
 
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,30 @@
 import "./topbar.scss"
+import PersonIcon from '@mui/icons-material/Person';
+import MailIcon from '@mui/icons-material/Mail';
 
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
     return (
-        <div className="topbar">
-            
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">genius.</a>
+                    <div className="itemContainer">
+                        <PersonIcon className="icon"/>
+                        <span>+44 924 12 74</span>
+                    </div>
+                    <div className="itemContainer">
+                        <MailIcon className="icon"/>
+                        <span>dimon@dimon.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                   <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                       <span className="line1"></span>
+                       <span className="line2"></span>
+                       <span className="line3"></span>
+                   </div>
+                </div>
+            </div>
         </div>
     )
 }
