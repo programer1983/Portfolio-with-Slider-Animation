b0728dfaf52f34262e066c33b4791cfb9561b7b4
programer1983 <programer972@gmail.com>
2021-12-26 11:46:28 +0200

    adding a list of names, mapping it and forwarding it to a separate component;

---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,10 @@
+import "./portfolioList.scss"
+
+export default function PortfolioList({title}) {
+    return (
+        <li className="portfolioList">
+            {title}
+        </li>
+    )
+}
+
