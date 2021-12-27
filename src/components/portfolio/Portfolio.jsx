09a6417228a003486d0f74a7e9f2315daee51cf2
programer1983 <programer972@gmail.com>
2021-12-27 21:29:38 +0200

    mapping the database by categories, connecting categories using construction   switch,   case .

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,19 @@
 import PortfolioList from '../portfolioList/PortfolioList';
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import './portfolio.scss'
+import {
+    featuredPortfolio,
+    webPortfolio,
+    mobilePortfolio,
+    designPortfolio,
+    contentPortfolio,
+} from "./../../data"
+
+
 
 export default function Portfolio() {
   const [selected, setSelected] = useState("featured")
+  const [data, setData] = useState([])
     
   const list = [
         {
@@ -27,6 +37,28 @@ export default function Portfolio() {
           title: "Content",
         },
       ];
+
+      useEffect(() => {
+        switch(selected){
+          case "featured":
+            setData(featuredPortfolio)
+            break
+          case "web":
+            setData(webPortfolio)
+            break
+          case "mobile":
+            setData(mobilePortfolio)
+            break
+          case "design":
+            setData(designPortfolio)
+            break
+          case "content":
+            setData(contentPortfolio)
+            break
+            default:
+            setData(featuredPortfolio)
+        }
+      }, [selected])
     
     return (
         <div className="portfolio" id="portfolio">
@@ -42,30 +74,12 @@ export default function Portfolio() {
                ))}
             </ul>
             <div className="container">
+              {data.map((d) => (
                 <div className="item">
-                    <img src="https://mir-s3-cdn-cf.behance.net/projects/404/02762469817767.Y3JvcCwxMjUzLDk4MCwyNDIsNjU.png" alt="" />
-                    <h3>Banking App</h3>
+                  <img src={d.img} alt="" />
+                  <h3>{d.title}</h3>
                 </div>
-                <div className="item">
-                    <img src="https://mir-s3-cdn-cf.behance.net/projects/404/02762469817767.Y3JvcCwxMjUzLDk4MCwyNDIsNjU.png" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://mir-s3-cdn-cf.behance.net/projects/404/02762469817767.Y3JvcCwxMjUzLDk4MCwyNDIsNjU.png" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://mir-s3-cdn-cf.behance.net/projects/404/02762469817767.Y3JvcCwxMjUzLDk4MCwyNDIsNjU.png" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://mir-s3-cdn-cf.behance.net/projects/404/02762469817767.Y3JvcCwxMjUzLDk4MCwyNDIsNjU.png" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://mir-s3-cdn-cf.behance.net/projects/404/02762469817767.Y3JvcCwxMjUzLDk4MCwyNDIsNjU.png" alt="" />
-                    <h3>Banking App</h3>
-                </div>
+              ))}
             </div>
         </div>
     )
