0017dfbfcc852f0ce4aaba16b812ef38c9cab19b
programer1983 <programer972@gmail.com>
2021-12-29 20:02:22 +0200

    adding data for the slider and displaying them;

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,31 +1,64 @@
 import "./works.scss"
 
 export default function Works() {
+
+    const data = [
+        {
+            id: "1",
+            icon: "./assets/mobile.png",
+            title: "Web Design",
+            desc:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+            img:
+              "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+          },
+          {
+            id: "2",
+            icon: "./assets/globe.png",
+            title: "Mobile Application",
+            desc:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            img:
+              "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+          },
+          {
+            id: "3",
+            icon: "./assets/writing.png",
+            title: "Branding",
+            desc:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            img:
+              "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+          },
+    ]
+    
     return (
         <div className="works" id="works">
             <div className="slider">
+               { data.map((d) => (
                 <div className="container">
-                    <div className="item">
-                        <div className="left">
-                            <div className="leftContainer">
-                                <div className="imgContainer">
-                                    <img src="assets/mobile.png" alt="" />
-                                </div>
-                                <h2>Title</h2>
-                                <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget eros sed est pretium fermentum eu ac dolor. 
-                                </p>
-                                <span>Projects</span>
-                            </div>
-                        </div>
-                        <div className="right">
-                            <img 
-                              src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930" 
-                              alt="" 
-                            />
-                        </div>
-                    </div>
-                </div>
+                   <div className="item">
+                       <div className="left">
+                           <div className="leftContainer">
+                               <div className="imgContainer">
+                                   <img src={d.icon} alt="" />
+                               </div>
+                               <h2>{d.title}</h2>
+                               <p>
+                                {d.desc} 
+                               </p>
+                               <span>Projects</span>
+                           </div>
+                       </div>
+                       <div className="right">
+                           <img 
+                             src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930" 
+                             alt="" 
+                           />
+                       </div>
+                   </div>
+               </div>  
+               ))}
             </div> 
             <img src="assets/arrow.png" className="arrow left" alt="" />
             <img src="assets/arrow.png" className="arrow right" alt="" />
